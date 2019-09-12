2a18653fb7f253797a02b9623e3eafec36352f34
kyamisuke <koukou0301@gmail.com>
2019-09-12 14:36:18 +0900

    Initial Commit

---
+++ TouchStamp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TouchStamp
+//
+//  Created by 村上航輔 on 2019/09/12.
+//  Copyright © 2019 kyamisuke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
